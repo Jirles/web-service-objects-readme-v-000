56b6f0612d903714e95ee9ac55c7a5b01a64c399
Emily Jirles <emily.jirles@gmail.com>
2018-07-20 13:38:05 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,13 +4,14 @@ class SearchesController < ApplicationController
   end
 
   def friends
-    foursquare = Foursquare.new
+    foursquare = FoursquareService.new
     @friends = foursquare.friends(session[:token])
   end
 
   def foursquare
     client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
     client_secret = "0NNKMRWRYLCKLPSEE3G10I33WV0BTYXEN2JCJ41TVKKWB52Y"
+    foursquare = FoursquareService.new
     @resp = foursquare.coffee_shops(client_id, client_secret, params[:zipcode])
     body = JSON.parse(@resp.body)
     if @resp.success?
